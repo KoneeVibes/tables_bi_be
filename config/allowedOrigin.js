7f1909ef3d4d69b42ad339e5b0a2aa5cc1530b60
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-07-22 03:54:20 +0100

    updating allowed origin to include deployed url

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
     "http://192.168.0.152:2701",
     "http://localhost:2701",
     "http://localhost:8971",
+    "https://tables-bi-fe.onrender.com",
+    "https://tables-bi-be.onrender.com",
 ];
 
 module.exports = allowedOrigins;
