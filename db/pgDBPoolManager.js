c4b500f4fc27c235d6b90c46283f0ce409ccff20
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-08-24 18:29:10 +0100

    setting ssl requirement to false

--- db/pgDBPoolManager.js
+++ db/pgDBPoolManager.js
@@ -1,26 +1,35 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pools = new Map();
 
 function getDBPool({ user, host, database, password, port }) {
-    const key = `${host}_${database}_${user}`;
+	const key = `${host}_${database}_${user}`;
 
-    if (pools.has(key)) {
-        return pools.get(key);
-    };
+	if (pools.has(key)) {
+		return pools.get(key);
+	}
 
-    const pool = new Pool({ user, host, database, password, port });
+	const pool = new Pool({
+		user,
+		host,
+		database,
+		password,
+		port,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
 
-    pool.on('connect', () => {
-        console.log(`Connected to DB: ${database}`);
-    });
+	pool.on("connect", () => {
+		console.log(`Connected to DB: ${database}`);
+	});
 
-    pool.on('error', (err) => {
-        console.error(`Unexpected error on DB: ${database}`, err);
-    });
+	pool.on("error", (err) => {
+		console.error(`Unexpected error on DB: ${database}`, err);
+	});
 
-    pools.set(key, pool);
-    return pool;
+	pools.set(key, pool);
+	return pool;
 }
 
 module.exports = getDBPool;
